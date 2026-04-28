274747c0b2224acd0c09109b979da5cbf53328c1
HamzaAbdiaziz <cabdihamza851@gmail.com>
2026-04-28 13:00:50 +0300

    updated

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,7 +3,11 @@ import {calculateInvestmentResults} from "../util/investment.js";
 import {formatter} from "../util/investment.js";
 
 export default function Results({ input }) {
-    const resultsData = calculateInvestmentResults(input);
+const resultsData = calculateInvestmentResults(input);
+
+if (resultsData.length === 0) {
+    return <p className="center">Invalid input data provided</p>;
+}
 
 const initialInvestment = resultsData[0].valueEndOfYear -
 resultsData[0].interest -
